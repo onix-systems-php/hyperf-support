b3acbc9332ca66badb36018d32a9debeb04d0452
Ihor Rud <Igorryd7@gmail.com>
2024-04-02 16:54:54 +0300

    feat(events): add ticket updated event

---
+++ src/Events/TicketUpdated.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of the extension library for Hyperf.
+ *
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace OnixSystemsPHP\HyperfSupport\Events;
+
+use OnixSystemsPHP\HyperfSupport\Contract\TicketEvent;
+use OnixSystemsPHP\HyperfSupport\Model\Ticket;
+
+class TicketUpdated implements TicketEvent
+{
+    public function __construct(public Ticket $ticket) {}
+}
