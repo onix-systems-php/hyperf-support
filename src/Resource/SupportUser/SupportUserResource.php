74e9b19c2a8e206f0c518ed6c3b4e5186ecda2d0
Ihor Rud <Igorryd7@gmail.com>
2024-04-08 13:39:03 +0300

    fix(tickets): rename resource

--- src/Resource/SupportUser/SupportUserResource.php
+++ src/Resource/SupportUser/SupportUserResource.php
@@ -1,24 +1,24 @@
 <?php
 
-namespace OnixSystemsPHP\HyperfSupport\Resource\User;
+namespace OnixSystemsPHP\HyperfSupport\Resource\SupportUser;
 
 use OnixSystemsPHP\HyperfCore\Resource\AbstractResource;
 use OnixSystemsPHP\HyperfSupport\Contract\SupportUserInterface;
 use OpenApi\Attributes as OA;
 
-#[OA\Schema(schema: 'UserResource', properties: [
-    new OA\Property(property: 'email', type: 'integer'),
+#[OA\Schema(schema: 'SupportUserResource', properties: [
+    new OA\Property(property: 'id', type: 'string'),
     new OA\Property(property: 'first_name', type: 'string'),
     new OA\Property(property: 'last_name', type: 'string'),
     new OA\Property(property: 'full_name', type: 'string'),
-    new OA\Property(property: 'id', type: 'string'),
+    new OA\Property(property: 'email', type: 'string'),
 ], type: 'object')]
 
 /**
  * @method __construct(SupportUserInterface $resource)
  * @property SupportUserInterface $resource
  */
-class UserResource extends AbstractResource
+class SupportUserResource extends AbstractResource
 {
     public function toArray(): array
     {
--- src/Resource/Ticket/TicketResource.php
+++ src/Resource/Ticket/TicketResource.php
@@ -4,7 +4,7 @@ namespace OnixSystemsPHP\HyperfSupport\Resource\Ticket;
 
 use OnixSystemsPHP\HyperfCore\Resource\AbstractResource;
 use OnixSystemsPHP\HyperfSupport\Model\Ticket;
-use OnixSystemsPHP\HyperfSupport\Resource\User\UserResource;
+use OnixSystemsPHP\HyperfSupport\Resource\SupportUser\SupportUserResource;
 use OpenApi\Attributes as OA;
 
 #[OA\Schema(schema: 'TicketResource', properties: [
@@ -13,9 +13,9 @@ use OpenApi\Attributes as OA;
     new OA\Property(property: 'content', type: 'string'),
     new OA\Property(property: 'source', type: 'string'),
     new OA\Property(property: 'custom_fields', type: 'object'),
-    new OA\Property(property: 'creator', schema: '#/components/schemas/UserResource'),
-    new OA\Property(property: 'modifier', schema: '#/components/schemas/UserResource'),
-    new OA\Property(property: 'archiver', schema: '#/components/schemas/UserResource'),
+    new OA\Property(property: 'creator', schema: '#/components/schemas/SupportUserResource'),
+    new OA\Property(property: 'modifier', schema: '#/components/schemas/SupportUserResource'),
+    new OA\Property(property: 'archiver', schema: '#/components/schemas/SupportUserResource'),
     new OA\Property(property: 'completed_at', type: 'string'),
     new OA\Property(property: 'trello_url', type: 'string'),
     new OA\Property(property: 'page_url', type: 'string'),
@@ -37,9 +37,9 @@ class TicketResource extends AbstractResource
             'content' => $this->resource->content,
             'source' => $this->resource->source,
             'custom_fields' => $this->resource->custom_fields,
-            'creator' => UserResource::make($this->resource->creator),
-            'editor' => UserResource::make($this->resource->editor),
-            'archiver' => UserResource::make($this->resource->archiver),
+            'creator' => SupportUserResource::make($this->resource->creator),
+            'editor' => SupportUserResource::make($this->resource->editor),
+            'archiver' => SupportUserResource::make($this->resource->archiver),
             'completed_at' => $this->resource->completed_at,
             'trello_url' => $this->resource->trello_url,
             'page_url' => $this->resource->page_url,
